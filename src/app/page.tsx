1188cafbe5c82e6650e1cd11c71a41d1f04f0e0a
Azharul Islam <azharulip@gmail.com>
2026-04-10 23:04:05 +0600

    Sroll effect added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,44 @@
+"use client";
+
 import Tech from "@/components/teck/Tech";
 import Hero from "../components/Hero/Hero";
 import ProjectTwo from "@/components/Projects/ProjectTwo";
 import Experience from "@/components/Experience/Experience";
 import Contact from "@/components/contact/Contact";
 
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const sectionId = searchParams.get("sectionId");
+
+    if (!sectionId) return;
+
+    let attempts = 0;
+
+    const tryScroll = () => {
+      const section = document.getElementById(sectionId);
+
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      } else if (attempts < 10) {
+        attempts++;
+        setTimeout(tryScroll, 100);
+      }
+    };
+
+    setTimeout(tryScroll, 100);
+  }, [searchParams]);
   return (
-    
     <div className="container mx-auto">
-        <Hero />
-        <Tech/>
-        <ProjectTwo/>
-        <Experience/>
-        <Contact/>
+      <Hero />
+      <Tech />
+      <ProjectTwo />
+      <Experience />
+      <Contact />
     </div>
-   
   );
 }
