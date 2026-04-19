311428d168ec249f6457c5d2f8a03b8afbc659b5
Azharul Islam <azharulip@gmail.com>
2026-04-19 07:10:41 +0600

    Packcage updated and few performence optimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
-/** @type {import('next').NextConfig} */
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "azharul-islam-b901c.web.app",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "azharul-islam-b901c.web.app",
       },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
